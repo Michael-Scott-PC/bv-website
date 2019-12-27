6f50322bae4971c277d2a0311dec736b7ebecef1
Mike Enochs <msenochs@umich.edu>
2019-12-27 16:00:07 -0500

    Update axiosStrapi.js

--- frontend/src/api/axiosStrapi.js
+++ frontend/src/api/axiosStrapi.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:1337'
+    : '157.245.14.110:1337';
+
 export default axios.create({
-  baseURL: 'http://localhost:1337'
+  baseURL: `${baseURL}`
 });
