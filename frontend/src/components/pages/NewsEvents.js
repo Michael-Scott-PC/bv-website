c45bec48d684e6d28445a396f3686e3ee6c86ab7
Mike Enochs <msenochs@umich.edu>
2020-01-09 22:35:21 -0500

    Updated listing model & added conditional to open house section
    I changed garage field type from int to string since Brian needs to be able to add descriptions (e.g. "detached").  Added a check to see if the retrieved open_house date is > than current date - if it's not, then it is not displayed.  Unfortunately, Strapi does not allow you to clear the date once it has been set, you can change it, but not clear it, so this was the best solution at the moment to get rid of an open house that was set for example purposes.

--- frontend/src/components/pages/NewsEvents.js
+++ frontend/src/components/pages/NewsEvents.js
@@ -1,5 +1,5 @@
 import './NewsEvents.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 
@@ -33,44 +33,71 @@ const NewsEvents = ({
   }, [getAllListings]);
 
   const renderOpenHouses = i => {
-    const jsx = [<h1 className='ml-4 mt-5 col-12'>Upcoming Open Houses</h1>];
+    const now = new Date();
+    console.log(now);
+    console.log(typeof now);
+    const jsx = [
+      <h1 key={'open-house-header'} className='ml-4 mt-5 col-12'>
+        Upcoming Open Houses
+      </h1>
+    ];
     for (i = 0; i < allListings.length; i++) {
       if (allListings[i].open_house) {
-        jsx.push(
-          <div
-            key={allListings[i].id}
-            className='card container-fluid open-house-card my-4'
-          >
-            <div className='row open-house-row'>
-              <div className='col-5 col-md-4 open-house-img-col'>
-                <img
-                  className='card-img-top cover-thumbnail'
-                  src={`${process.env.REACT_APP_STRAPIURL}${allListings[i].cover_photo.url}`}
-                  alt='property for sale'
-                  style={{
-                    width: '100%',
-                    height: '120px',
-                    objectFit: 'fill'
-                  }}
-                />
-              </div>
-              <div className='col-7 col-md-8 open-house-info-col'>
-                <div className='card-header open-house-text'>
-                  {allListings[i].address}
-                  <br />
-                  {allListings[i].city}, {allListings[i].state}{' '}
-                  {allListings[i].zipcode}
-                  <br />
-                  {moment(allListings[i].open_house).format('MM-DD-YYYY')}{' '}
-                  <br />
-                  {moment(allListings[i].open_house).format('h:mma')} -{' '}
-                  {moment(allListings[i].open_house_end_time).format('h:mma')}
+        console.log(allListings[i].open_house);
+        console.log(typeof allListings[i].open_house);
+        const converted_date = new Date(allListings[i].open_house);
+        if (converted_date > now) {
+          jsx.push(
+            <div
+              key={allListings[i].id}
+              className='card container-fluid open-house-card my-4'
+            >
+              <div className='row open-house-row'>
+                <div className='col-5 col-md-4 open-house-img-col'>
+                  <img
+                    className='card-img-top cover-thumbnail'
+                    src={`${process.env.REACT_APP_STRAPIURL}${allListings[i].cover_photo.url}`}
+                    alt='property for sale'
+                    style={{
+                      width: '100%',
+                      height: '120px',
+                      objectFit: 'fill'
+                    }}
+                  />
+                </div>
+                <div className='col-7 col-md-8 open-house-info-col'>
+                  <div className='card-header open-house-text'>
+                    {allListings[i].address}
+                    <br />
+                    {allListings[i].city}, {allListings[i].state}{' '}
+                    {allListings[i].zipcode}
+                    <br />
+                    {moment(allListings[i].open_house).format(
+                      'MM-DD-YYYY'
+                    )}{' '}
+                    <br />
+                    {moment(allListings[i].open_house).format('h:mma')} -{' '}
+                    {moment(allListings[i].open_house_end_time).format('h:mma')}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
+          );
+        }
       }
+    }
+    if (jsx.length === 1) {
+      return (
+        <Fragment>
+          <h1 key={'open-house-header'} className='ml-4 mt-5 col-12'>
+            Upcoming Open Houses
+          </h1>
+          <p style={{ color: '#000000' }}>
+            There are currently no scheduled open houses. Please check back at a
+            later date or contact Brian for any open house information.
+          </p>
+        </Fragment>
+      );
     }
     return jsx;
   };
