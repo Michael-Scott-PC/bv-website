52a5460bf29dd16d672668221fc94ffae332f641
Mike Enochs <msenochs@umich.edu>
2020-01-15 18:46:01 -0500

    Update types.js

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -22,3 +22,5 @@ export const LOGOUT = 'LOGOUT';
 export const GOOGLE_SIGN_IN = 'GOOGLE_SIGN_IN';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const GOOGLE_USER_LOADED = 'GOOGLE_USER_LOADED';
+export const GET_LISTINGS_COUNT = 'GET_LISTINGS_COUNT';
